953b26e55ac4a9f8385dd81d88404522609bfb15
yousufahmed12 <thuinahmed@yahoo.com>
2017-11-06 14:31:35 -0500

    Update universityupload.php

--- FinishedMidAssesment2/php/universityupload.php
+++ FinishedMidAssesment2/php/universityupload.php
@@ -65,7 +65,7 @@ if(isset($_POST['submit1'])){
         $_SESSION['message'] = '';
         $servername = "localhost";
         $user = "root";
-        $passwd = "Liger124!";
+        $passwd = "";
         $dbname ="accounts";
         
         $runiversity = $_POST['university'];
