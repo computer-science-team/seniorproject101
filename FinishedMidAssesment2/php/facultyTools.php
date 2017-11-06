244dc2aa6de686610ec80326857f41da189895f8
yousufahmed12 <thuinahmed@yahoo.com>
2017-11-06 14:22:23 -0500

    Update facultyTools.php

--- FinishedMidAssesment2/php/facultyTools.php
+++ FinishedMidAssesment2/php/facultyTools.php
@@ -13,7 +13,7 @@ $faculty = $_SESSION['faculty'];
 //echo $faculty;
 //echo $faculty;
 $user = "root";
-$passwd = "Liger124!";
+$passwd = ";
 $dbname ="accounts";
 $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 // Check connection
