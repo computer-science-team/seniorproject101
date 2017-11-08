64c51673f4a5b7fe7f59af8f1c28807554356be2
yousufahmed12 <thuinahmed@yahoo.com>
2017-11-08 14:48:46 -0500

    Update changeEmail.php

--- FinishedMidAssesment2/php/changeEmail.php
+++ FinishedMidAssesment2/php/changeEmail.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $id = ($_SESSION['id']);
+$role = ($_SESSION['role']);
 $servername = "localhost";
 $user = "root";
 $passwd = "";
@@ -26,10 +27,16 @@ else
         
         if ($mysqli->query($sql) === TRUE)
         {
-            echo 'Email successfully updated! 
-You will be redirected to the Log In page';
-            //header("location: signinpage.php");
-            header( "refresh:4; url = profilePage.php" );
+            echo 'Email successfully updated! You will be redirected to the Log In page';
+	 $role2 = "yes";
+            
+	 if (strcmp($role, $role2) !== 0){
+        		header("location:studentProfilePage.php");
+			
+			}
+	 else{
+		header("location:facultyProfilePage.php");
+		    }
         } 
         else
         {
