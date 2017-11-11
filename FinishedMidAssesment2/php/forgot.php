5f1a4a44228273057759971b791c23fbfab98e1f
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:30:35 -0500

    small change

--- FinishedMidAssesment2/php/forgot.php
+++ FinishedMidAssesment2/php/forgot.php
@@ -40,9 +40,11 @@ if(isset($_POST['submit']))
 
 
 <!doctype html>
-<html>
-	<head>
-	<meta charset="utf-8">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Change Password</title>
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
         <link href="../css/styles.css" rel="stylesheet">
