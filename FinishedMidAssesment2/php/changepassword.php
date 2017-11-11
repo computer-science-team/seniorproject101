4123c5730f917edd9fb3eb1daa63d68eb07043f4
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:21:55 -0500

    small change

--- FinishedMidAssesment2/php/changepassword.php
+++ FinishedMidAssesment2/php/changepassword.php
@@ -49,9 +49,11 @@ You will be redirected to the Log In page';
 
 
 <!doctype html>
-<html>
-	<head>
-	<meta charset="utf-8">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Change Password</title>
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
         <link href="../css/styles.css" rel="stylesheet">
