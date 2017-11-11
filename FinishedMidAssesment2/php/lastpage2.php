932decdf51426413c5e7e55fad673aaaf7cf5f0e
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:39:52 -0500

    small change

--- FinishedMidAssesment2/php/lastpage2.php
+++ FinishedMidAssesment2/php/lastpage2.php
@@ -111,11 +111,8 @@ if(isset($_POST['Add'])){
 ?>
 <!DOCTYPE html>
 
-<html>
-		
-	<html>
-		
-	<head>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
