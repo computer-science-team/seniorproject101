57825df8276bdcd8600d380b2c6c3143c055ac2b
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:45:09 -0500

    small change

--- FinishedMidAssesment2/php/login.php
+++ FinishedMidAssesment2/php/login.php
@@ -91,9 +91,11 @@ if(isset($_POST['signin']))
  ?>
  
  <!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Login</title>
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
         <link href="../css/styles.css" rel="stylesheet">
