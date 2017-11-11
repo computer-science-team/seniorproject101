d196a77216cd5f123abf6c05a3dd0ccb1fc3a8c9
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:49:57 -0500

    small change

--- FinishedMidAssesment2/php/universitysearch.php
+++ FinishedMidAssesment2/php/universitysearch.php
@@ -42,8 +42,8 @@ if(isset($_POST['view'])){
 ?>
    
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
