433b9f21a2438cf36e30bcd7151a73b19c7fa936
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-11 15:29:44 -0500

    small change

--- FinishedMidAssesment2/php/facultyTools.php
+++ FinishedMidAssesment2/php/facultyTools.php
@@ -95,9 +95,8 @@ if(isset($_POST['Add'])){
 
 <html>
 		
-	<html>
-		
-	<head>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
