9de9fb94213b26216d2ecce32840e82f926cc5a4
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-12 00:03:56 -0500

    update by krunal with rogers code in mind

--- FinishedMidAssesment2/php/universitydownload2.php
+++ FinishedMidAssesment2/php/universitydownload2.php
@@ -1,65 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UniversityDownloadpage</title>
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/styles.css" rel="stylesheet">
-    
-    </head>
-	<body>
-        <nav class="navbar navbar-inverse navbar-static-top">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">Download</a>
-		</div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav navbar-right">
-                
-				
-                <li><a href="../index.html">Logout</a></li>
-			</ul>
-		</div>
-	</div>
-    </nav>
-        <div class="jumbotron">
-       <div class="container">
-		<div class="universitydownload">
-			<h2>Download Your University's Guidelines for Success</h2>
-				<form method="post">
-					<p>Enter the name of your University: </p>
-					<input type="text" name="university" placeholder="university" value="<?php if(isset($_POST['university'])){ echo $_POST['university'];} ?>"/>
-					<input type="submit" name="submit" value="Submit">
-				</form>
-		</div>
-            </div>
-        </div>
-       <script src="../js/jquery-3.2.1.min.js"></script>
-        <script src="../js/bootstrap.min.js"></script> 
-	</body>
-</html>
-<?php 
-if(isset($_POST['submit'])){ 
 
-    if (!$_POST['university']){
-        echo "<br/>-Please enter the name of your university";
-    }
-        if(isset($_POST['university'])){
+
+<?php 
             session_start();//starts session
             $_SESSION['message'] = '';
             $servername = "localhost";
             $user = "root";
-            $passwd = "";
+            $passwd = "kkp123";
             $dbname ="accounts";
-            $runiversity = $_POST['university'];
+            $runiversity = $_SESSION['university'];
             $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
             // Check connection
             if ($mysqli->connect_error) {
@@ -81,13 +29,13 @@ if(isset($_POST['submit'])){
                 }
                 $runivid=$_SESSION['runivid'];
                 //search for file in database
-                $selectFirstQuery = "SELECT guide_fname,guide_path FROM universities WHERE univid  = '". $runivid ."' AND guide_path IS NOT NULL";
+                $selectFirstQuery = "SELECT club_fname,club_path FROM universities WHERE univid  = '". $runivid ."' AND club_path IS NOT NULL";
                 $queryResult = $mysqli->query($selectFirstQuery);
             	$foundRows = $queryResult->num_rows;
                 if($foundRows > 0){  
                 	while($row = mysqli_fetch_assoc($queryResult)){
-            			$path = $row['guide_path'];
-               			$file_name = $row['guide_fname'];
+            			$path = $row['club_path'];
+               			$file_name = $row['club_fname'];
             	   
                 	}
                 	echo "found file";
@@ -98,15 +46,13 @@ if(isset($_POST['submit'])){
                     exit;
                 }
             }
+            //no list of clubs is available-redirect to profile page with message 
             if($foundRows===0){
-                	echo "<br/>No Guidelines for Success document has been uploaded";
-            	}
+                $_SESSION['message'] = "No list of clubs is avilable for ".$runiversity."";
+                header("location:studentProfilePage.php");
+                
             //Row not found-file not in database
-           
-    }//post university
-    else{
-            	echo "<br/>Your college is not in our database";
-            	
-  	}
-}//isset
+            }
+   
+   
 ?>
