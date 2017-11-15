02d8a268e9d389b30ab48fec7f07cad2176fbbaf
morrisr0 <morrisr0@students.rowan.edu>
2017-11-15 17:01:58 -0500

    Add files via upload

---
+++ FinishedMidAssesment2/11152017 DeleteTools.php/deleteTools.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+$id = ($_SESSION['id']);
+
+$username = ($_SESSION['username']);
+$runiversity= ($_SESSION['university']);
+$runivid= ($_SESSION['univid']);
+
+//Prints tool removed
+if (!empty($_SESSION['message'])) {
+    echo '<p class="message"> '.$_SESSION['message'].'</p>';
+    unset($_SESSION['message']);
+}
+$servername = "localhost";
+$user = "root";
+$passwd = "";
+$dbname ="accounts";
+$mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+    //echo "Connected successfully";
+}
+if(isset($_POST['Delete'])){
+    echo "pressed";
+    //echo "pressed";
+}
+//goes url in student database
+if(isset($_POST['Visit'])){
+    //echo "pressed";
+    echo $_POST['Visit'];
+    $var=$_POST['Visit'];
+    
+    header('location:' . $var);
+    
+}
+
+
+//Deletes tool to student's tool kit
+if(isset($_POST['Delete'])){
+    
+    $var=($_POST['Delete']);
+    $selectFirstQuery = "DELETE FROM tools WHERE url  = '". $var ."' AND idnums = '".$id."'";
+    
+    if ($mysqli->query($selectFirstQuery) === TRUE) {
+        //echo "<br/> ".$var. " has been deleted from your toolkit";
+        $_SESSION['message'] = "'".$var."' has been deleted from your tool kit";
+        //header("location:deleteTools.php");
+        header("Refresh:0");
+    } else {
+        echo "Error deleting record: " . $mysqli->error;
+    }
+    
+    
+    
+}
+
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+		
+	<html>
+		
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>StudentToolpage</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/styles.css" rel="stylesheet">
+    
+</head>
+    <body>
+    <nav class="navbar navbar-inverse navbar-static-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="lastpage.php"><?php $_SESSION['id']=$id;
+		$_SESSION['username']=$username; $_SESSION['runiversity']=$runiversity;  $_SESSION['runivid']=$runivid; ?>View Toolkit</a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile<span class="caret"></span></a>
+				<ul class="dropdown-menu">
+				  
+				  <li><a href="studentProfilePage.php">Profile Page</a></li>
+				  <li><a href="universitysearch.php"><?php $_SESSION['id']=$id;
+		$_SESSION['username']=$username; $_SESSION['runiversity']=$runiversity;  $_SESSION['runivid']=$runivid; ?>Search All Universities</a></li>
+                  <li><a href="facultySearch.php"><?php $_SESSION['id']=$id;
+		$_SESSION['username']=$username; $_SESSION['runiversity']=$runiversity;  $_SESSION['runivid']=$runivid; ?>Faculty Search</a></li>
+                 
+				</ul>
+			  </li>
+                <li><a href="../index.html">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+      
+	
+      
+	  	<div class="jumbotron">
+        <div class="container">  
+		<div class="loginBox">
+		<h3> <?php echo $username;?> , here are the tools available for deletion:</h3>
+		 <form method="post">
+		
+		<?php
+		//Display user's toolkit
+		$selectFirstQuery = "SELECT * FROM tools WHERE idnums  = '". $id ."'";
+		$queryResult = $mysqli->query($selectFirstQuery);
+		$foundRows = $queryResult->num_rows;
+		//checks if anything in tool kit
+		if($foundRows > 0)
+		{
+		    
+		    echo "<table border='1'>";
+		    echo "<tr><td>Name</td><td>Category</td><td>Delete from toolkit</td><rr>";
+		    while($row=mysqli_fetch_assoc($queryResult)){
+		        
+		           
+		        //echo "<tr><td>{$row['toolname']}</td><td>{$row['category']}</td><td>{$row['url']}</td><td>{$row['url']}</td>";
+		        echo "<tr><td>{$row['toolname']}</td><td>{$row['category']}</td><td><input type='submit' name='Delete' value = {$row['url']}></td>";
+		            
+		    }
+		   
+		}//ifr] rows
+		//User toolkit
+		
+		?>
+		
