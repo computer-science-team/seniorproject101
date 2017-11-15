379d6f8dee49231870c7dc6d99f511eb922169a9
christenc3 <31932938+christenc3@users.noreply.github.com>
2017-11-15 16:39:04 -0500

    Update universityTools.php

--- FinishedMidAssesment2/php/universityTools.php
+++ FinishedMidAssesment2/php/universityTools.php
@@ -8,7 +8,7 @@ $runivid= ($_SESSION['univid']);
 //echo $username;
 $servername = "localhost";
 $user = "root";
-$passwd = "Liger124!";
+$passwd = "";
 $dbname ="accounts";
 $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 // Check connection
