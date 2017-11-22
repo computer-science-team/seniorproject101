5e0ed97ac796c60f0a0aaaefbc416f2880bc8ea7
yousufahmed12 <thuinahmed@yahoo.com>
2017-11-21 22:19:21 -0500

    Update universityTools.php

--- FinishedMidAssesment2/lastPage/universityTools.php
+++ FinishedMidAssesment2/lastPage/universityTools.php
@@ -9,7 +9,7 @@ $role = ($_SESSION['role']);
 //echo $username;
 $servername = "localhost";
 $user = "root";
-$passwd = "Liger124!";
+$passwd = "";
 $dbname ="accounts";
 $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 // Check connection
