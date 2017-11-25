106da72ef7396b0933ca75e6fdd90d77b5db60bd
KrunalPatel123 <krunalpatel173@gmail.com>
2017-11-24 21:58:43 -0500

    rm pass

--- preliminary Assesment/php/universityupload2.php
+++ preliminary Assesment/php/universityupload2.php
@@ -55,7 +55,7 @@ $runiversity = $_SESSION['runiversity'];
 $_SESSION['message'] = '';
 $servername = "localhost";
 $user = "root";
-$passwd = "kkp123";
+$passwd = "";
 $dbname ="accounts";
 $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 // Check connection
