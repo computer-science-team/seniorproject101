10e7ab6bc509af76adb4acf63f265b374b56793a
Dharmik Pandya <31775168+pandyad0@users.noreply.github.com>
2017-11-27 03:27:43 -0500

    Update popup.php
    Changed title of the page

--- preliminary Assesment/php/popup.php
+++ preliminary Assesment/php/popup.php
@@ -5,7 +5,7 @@ $pageStart = '<!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Dialog - Default functionality</title>
+  <title>Oops.. Something went wrong.</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
