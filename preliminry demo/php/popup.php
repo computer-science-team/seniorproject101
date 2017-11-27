d2b59f3eefdc85b5e59f38f079f6a767d6fc3cf7
Dharmik Pandya <31775168+pandyad0@users.noreply.github.com>
2017-11-27 15:37:44 -0500

    Update popup.php
    Merging new file

--- preliminry demo/php/popup.php
+++ preliminry demo/php/popup.php
@@ -5,7 +5,7 @@ $pageStart = '<!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Dialog - Default functionality</title>
+  <title></title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
@@ -26,7 +26,6 @@ $pageStart = '<!DOCTYPE html>
 </body>
 </html>';
 
-
 $pageStart1 = str_replace(
 '<div id="dialog" title="Basic dialog">
   <p>Basic dialog</p>
@@ -46,16 +45,148 @@ $pageStart2 = str_replace(
 </div>'
 , $pageStart);
 
-$pageStart3 = str_replace(
+$loginerror = str_replace(
 '<div id="dialog" title="Basic dialog">
   <p>Basic dialog</p>
 </div>',
-'<div id="dialog" title="Basic dialog">
-  <p>University is not found</p>
+'<div id="dialog" title="Error.. ">
+  <p>Username/password was incorrect.. Please try again with correct credentials. </p>
 </div>'
 , $pageStart);
 
+$facultyToolsPageToolAlreadyExistError = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Oops..">
+  <p>Tool already exist in your toolkit, try adding different tool. </p>
+</div>'
+, $pageStart);
 
+$passwordSuccessfullyChanged = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Success!">
+  <p>Your password was changed successfully.</p>
+</div>'
+, $pageStart);
 
+$passwordDoNotMatch = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>Password do not match</p>
+</div>'
+, $pageStart);
+
+$passwordCannotChange = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>We were unable to change your password, please try again. </p>
+</div>'
+, $pageStart);
+
+$namesDoNotMatch = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>Names, do not match. Please try again. </p>
+</div>'
+, $pageStart);
+
+$namesMatched = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Success">
+  <p>Your name has been changed.</p>
+</div>'
+, $pageStart);
+
+$emailChangedSuccessfully = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Success">
+  <p>Your email has been successfully updated.</p>
+</div>'
+, $pageStart);
+
+$emailCannotChangeError = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>We were unable to change the email. Pelase try again with correct email in both boxes.</p>
+</div>'
+, $pageStart);
+
+$genderChangeSuccessfully = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Success">
+  <p>Your gender has been successfully updated.</p>
+</div>'
+, $pageStart);
+
+$genderChangeError = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>We were unable to change gender. Pelase try again.</p>
+</div>'
+, $pageStart);
+
+$dateChangedSuccessfully = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Success">
+  <p>Your birthdate has been successfully updated.</p>
+</div>'
+, $pageStart);
+
+$changeDateError = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>We were unable to change your birthdate. Pelase try again.</p>
+</div>'
+, $pageStart);
+
+$signupErrorLast = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>We were unable to sign you up. Please try again. </p>
+</div>'
+, $pageStart);
+
+$signupUserAlreadyExist = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>Username already exist, please choose different username and try again.</p>
+</div>'
+, $pageStart);
+
+$signupEmailAlreadyExist = str_replace(
+'<div id="dialog" title="Basic dialog">
+  <p>Basic dialog</p>
+</div>',
+'<div id="dialog" title="Error">
+  <p>Email already exist, please choose different email and try again.</p>
+</div>'
+, $pageStart);
 
 ?>
