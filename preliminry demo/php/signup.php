a1c7ae1c14b354e7b892f03aea23a1c0603872e6
yousufahmed12 <thuinahmed@yahoo.com>
2017-12-02 21:33:05 -0500

    signup add ajax

--- preliminry demo/php/signup.php
+++ preliminry demo/php/signup.php
@@ -1,116 +1,92 @@
 <!-- PHP code which will connect to database and that way a user can sign up -->
 <?php
-if (isset($_POST['submit'])){
-session_start();//starts session
- $univid=$_SESSION['univid'];
- $university=$_SESSION['university'];
-$servername = "localhost";
-$user = "root";
-$passwd = "rowanphysicssweng";
-$dbname ="accounts";
-$mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
-$checked = (isset($_POST['optradio']))?true:false;
-if (!$_POST['fullname']){
-   echo "<br/>-Please enter your name";
-}
-if (($checked==false)&&(!$_POST['fullname'])){
-    echo "<br/>-Please select your gender";
-}
-if (($checked==false)&&($_POST['fullname'])){
-    echo "<br/>-Please select your gender";
-}
-if (!$_POST['dob']){
-    echo "<br/>-Please enter your birthdate";
-}
-if (!$_POST['email']){
-    echo "<br/>-Please enter your email";
-}
-if (!$_POST['username']){
-    echo "<br/>-Please enter your username";
-}
-if (!$_POST['password']){
-    echo "<br/>-Please enter your password";
-}
-if (!$_POST['role']){
-    echo "<br/>-Please enter your role";
-}
-if(isset($_POST['submit']) && isset($_POST['optradio'])){
-    
-    $fullname = $_POST['fullname'];
-    $gender=$_POST['optradio'];
-    $dob=$_POST['dob'];
-    $username = $_POST['username'];//allow html to insert username
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);//hashes password
-    $role = $_POST['role']; 
-    //Check if email is in use
-    
-    $selectFirstQuery = "SELECT * FROM users WHERE email  = '". $email ."'";
-    $queryResult = $mysqli->query($selectFirstQuery);
-    $foundRows = $queryResult->num_rows;
-    //if row is found email is in use
-    if($foundRows > 0)
-    {
-        include 'popup.php';
-		print $signupEmailAlreadyExist;
-    }
-    //Check to see if username is in use
-    $selectFirstQuery = "SELECT * FROM users WHERE username  = '". $username ."'";
-    $queryResult = $mysqli->query($selectFirstQuery);
-    $foundRows = $queryResult->num_rows;
-    if($foundRows > 0)
-    {
-        include 'popup.php';
-		print $signupUserAlreadyExist;
-    }
-    //Insert info into table-user not in use
-    $sql = "INSERT INTO users(name, dob, gender, username, email, password, faculty, univid)"
-        . "VALUES ('$fullname','$dob', '$gender','$username','$email','$password','$role','$univid')";
-        if ($mysqli->query($sql)==true)
-        {
-	    $selectFirstQuery = "SELECT id FROM users WHERE username  = '". $username ."'";
-            $queryResult = $mysqli->query($selectFirstQuery);
-            $foundRows = $queryResult->num_rows;
-            if($foundRows > 0)
-            {
-                echo 'id found';
-                while($row=mysqli_fetch_assoc($queryResult)){
-                    $id=$row['id'];
-                }
-            }
-            $_SESSION['id']=$id;
-            $_SESSION['username']=$username;
-            $_SESSION['univid']=$univid;
-            $_SESSION['univeristy']=$university;
-            echo 'You will be redirected shortly';
-            $role2 = "yes";
-            
-            if (strcmp($role, $role2) !== 0){
-        		header("location:studentProfilePage.php");
-			
-			}
-		else{
-		header("location:facultyProfilePage.php");
-		    }
-        }
-        else
-        {
-            include 'popup.php';
-			//	print $signupErrorLast;
-            
-        }
-}
-}//post
+include 'popup.php';
+
 ?>
 <!-- PHP code ends here -->
 <!doctype html>
 <html lang="en">
   <head>
+
+
+<!------------------------------------------------------->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script>
+
+	$(document).ready(function(){
+	$('#userForm').submit(function(){
+     
+	var Name = $("#name").val();
+	var Gender = $("#genders").val();
+	var Dob = $("#dob").val();
+	var Email = $("#email").val();
+	var Username = $("#username").val();
+	var Password = $("#password").val();
+	var Role = $("#roles").val();
+	
+	if(Name && Gender && Dob && Email && Username && Password && Role )
+{
+
+	role2 = "yes";
+	var urlNext = "";
+
+	  if(Role == role2)
+	{
+		urlNext = "facultyProfilePage.php";
+	}
+	 else
+	{
+		urlNext = "studentProfilePage.php";
+	}
+
+	
+        $.post('signup_receiver.php', { name: Name, gender: Gender,  dob : Dob, email : Email, username : Username, password : Password, role : Role}, function(data){
+
+	if (data == 1) {
+	window.location.href = urlNext;
+        }
+	   else if(data == 2){
+	    var popup = <?php echo json_encode($signupUserAlreadyExist); ?>;
+            $('#response').html(popup);
+	}
+	else if(data == 3){
+	    var popup = <?php echo json_encode($signupEmailAlreadyExist); ?>;
+            $('#response').html(popup);
+	}
+	else if(data == 4){
+	    alert( "sql isn't insserting the database");
+	}
+	else if(data == 0){
+	    alert( "University session doesn't exist");
+	}
+	else{
+		alert( data );
+		//unknown errors
+	}
+
+        }).fail(function() {
+         
+            // just in case posting your form failed
+            alert( "Posting failed." );
+             
+        });
+}
+else
+{
+	 alert( "value is empty");
+}
+ 	
+        // to prevent refreshing the whole page page
+        return false;
+ 
+    });
+});
+</script>
+<!------------------------------------------------------->
+
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -120,6 +96,9 @@ if(isset($_POST['submit']) && isset($_POST['optradio'])){
 	</head>
 	<body class = "singinpage">
 	<!-- Page menu starts here -->
+
+	<div id='response'></div>
+
 		<nav class="navbar navbar-inverse">
 		  <div class="container-fluid">
 			<div class="navbar-header">
@@ -135,39 +114,35 @@ if(isset($_POST['submit']) && isset($_POST['optradio'])){
         <div class = "wrapper">
 		<div class="form-signin">
 			<h2 class="form-signin-heading">Sign Up!</h2>
-			<form method="post">
+			<form id='userForm'>
 				<p>Name:
-				<br><input type="text" name="fullname" placeholder="name" value="<?php if(isset($_POST['fullname'])){ echo $_POST['fullname'];} ?>" required></p>
+				<br><input type="text" name="name" id="name" placeholder="Name" /></p>
 				<p>Gender:
-				<br><label class="radio-inline">
-					<input type="radio" name="optradio"  value="Male"<?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Male') 
-					     echo ' checked="checked"'; ?>required >Male
-				</label>
-				<label class="radio-inline">
-					<input type="radio" name="optradio" value ="Female" <?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Female') 
-					     echo ' checked="checked"'; ?> required >Female
-				</label>
-				<label class="radio-inline">
-					<input type="radio" name="optradio" value="Other"  <?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Other') 
-					     echo ' checked="checked"'; ?>required >Other
-				</label></p>
+
+				<select id="genders" name="gender">
+  				<option value="Male">Male</option>
+  				<option value="Female">Female</option>
+  				<option value="Other">Other</option>
+				</select>
+
 				<p>Birthdate: 
-				<br><input type="date" name="dob" placeholder="MM/DD/YYYY" value="<?php if(isset($_POST['dob'])){ echo $_POST['dob'];} ?>" required ></p>
+				<br><input type="date" name="dob" id="dob" placeholder="MM/DD/YYYY" /></p>
 				<p>Email: 
-				<br><input type="email" name="email" placeholder="Email" value="<?php if(isset($_POST['email'])){ echo $_POST['email'];} ?>" required ></p>
+				<br><input type="email" name="email" id="email" placeholder="Email" /></p>
 				<p>Username: 
-				<br><input type="text" name="username" placeholder="Username" value="<?php if(isset($_POST['username'])){ echo $_POST['username'];} ?>" required ></p>
+				<br><input type="text" name="username" id="username" placeholder="Username" /></p>
 				<p>Password: 
-				<br><input type="Password" name="password" placeholder="*********" value="<?php if(isset($_POST['password'])){ echo $_POST['password'];} ?>" required ></p>
-                <p>Faculty:
-				<select name='role'>
-					<br><option value="no" <?php if (isset($_POST['role']) && $_POST['role'] == 'no') 
-					     echo ' selected="selected"'; ?> required >No</option>
-					<option value="yes"<?php if (isset($_POST['role']) && $_POST['role'] == 'yes') 
-					    echo ' selected="selected"'; ?> required >Yes</option>
-				</select></p>
+				<br><input type="password" name="password" id="password" placeholder="*********" /></p>
+                		<p>Faculty:
+				<select id="roles" name="role">
+				<option value="no">No</option>
+  				<option value="yes">Yes</option>
+				</select>
+
+
+
 				
-                <p><input type="submit" name="submit" value="Submit"> </p> 
+                <p><input type='submit' value='Submit' /></p> 
 				<p>Already have an account? <br> <a href="login.php"> Log In</a></p>
 				<p><a href="FirstPage.php">Go Back</a></p>
 			   
