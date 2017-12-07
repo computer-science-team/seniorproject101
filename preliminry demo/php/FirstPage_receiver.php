6da4e42df0dceff3c5fa4642358acfd3c5ada8fe
yousufahmed12 <thuinahmed@yahoo.com>
2017-12-06 20:29:03 -0500

    Update FirstPage_receiver.php

--- preliminry demo/php/FirstPage_receiver.php
+++ preliminry demo/php/FirstPage_receiver.php
@@ -1,67 +1,6 @@
 <?php
 session_start();
      $_SESSION['university'] = "$_POST[name]";
-
-if(isset($_SESSION['university'])){ 
-
-$servername = "localhost";
-$user = "root";
-$passwd = "Liger124!";
-$dbname ="accounts";
-$university = $_SESSION['university'];
-//var_dump($university);
-$mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-$selectFirstQuery = "SELECT univid FROM universities WHERE name  = '". $university ."'";
-$queryResult = $mysqli->query($selectFirstQuery);
-//$result = $selectFirstQuery->row();
-
-$foundRows = $queryResult->num_rows;
-//if row is found email is in use
-if($foundRows > 0)
-{   
-$_SESSION['university']= $university;
-echo 1;
- 
-    while($row = mysqli_fetch_assoc($queryResult)){
-    	$_SESSION['univid'] = $row['univid'];
-    }
-    //You will be redirected to the Sign up page.
-    
-}
-else{
-     //check if university is already registered
-    $selectFirstQuery = "SELECT * FROM universities WHERE name  = '". $university ."'";
-    $queryResult = $mysqli->query($selectFirstQuery);
-    $foundRows = $queryResult->num_rows;
-    //No university found - University is being registered
-    if($foundRows == 0)
-    {
-        $sql = "INSERT INTO universities(name)" . "VALUES ('$university')";
-       
-        if ($mysqli->query($sql)==true)
-        {   //university
-            $selectFirstQuery = "SELECT univid FROM universities WHERE name  = '". $university ."'";
-            $queryResult = $mysqli->query($selectFirstQuery);
-            $foundRows = $queryResult->num_rows;
-            //get university id num
-            if($foundRows > 0){
-                while($row=mysqli_fetch_assoc($queryResult)){
-                    $_SESSION['univid'] = $row['univid'];
-                } 
-            }
-        }
-    }
-echo 2;
-}
-}
-else
-{
+	$_SESSION['count'] = '1';
 echo 0;
-}
 ?>
