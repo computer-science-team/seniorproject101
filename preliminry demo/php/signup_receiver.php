47f12ffb9d51ccccfde11c6c0a2d9d66253b6343
yousufahmed12 <thuinahmed@yahoo.com>
2017-12-06 20:32:33 -0500

    Update signup_receiver.php

--- preliminry demo/php/signup_receiver.php
+++ preliminry demo/php/signup_receiver.php
@@ -3,84 +3,23 @@ session_start();
 
  //include 'popup.php';
 
-
+$_SESSION['count'] = '1';
 
 if(isset($_SESSION['university'])){ 
-$univid=$_SESSION['univid'];
-$university=$_SESSION['university'];
-$servername = "localhost";
-$user = "root";
-$passwd = "";
-$dbname ="accounts";
-$mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-    $fullname = "$_POST[name]";
-    $gender= "$_POST[gender]";
-    $dob= "$_POST[dob]";
-    $username = "$_POST[username]";//allow html to insert username
-    $email = "$_POST[email]";
-    $password = md5("$_POST[password]");//hashes password
-    $role = "$_POST[role]"; 
-    //Check if email is in use
-    
-    $selectFirstQuery = "SELECT * FROM users WHERE email  = '". $email ."'";
-    $queryResult = $mysqli->query($selectFirstQuery);
-    $foundRows = $queryResult->num_rows;
-    //if row is found email is in use
-    if($foundRows > 0)
-    {
-	echo 3;
-       //duplicate emails needs to be unique
-		//print $signupEmailAlreadyExist;
-    }
-    else
-	{
-    //Check to see if username is in use
-    $selectFirstQuery = "SELECT * FROM users WHERE username  = '". $username ."'";
-    $queryResult = $mysqli->query($selectFirstQuery);
-    $foundRows = $queryResult->num_rows;
-    if($foundRows > 0)
-    {
-	echo 2;
-        //duplicate users in database needs to be unique
-		//print $signupUserAlreadyExist;
-    }
-    else
-{
-    //Insert info into table-user not in use
-    $sql = "INSERT INTO users(name, dob, gender, username, email, password, faculty, univid)"
-        . "VALUES ('$fullname','$dob', '$gender','$username','$email','$password','$role','$univid')";
-        if ($mysqli->query($sql)==true)
-        {
-	    $selectFirstQuery = "SELECT id FROM users WHERE username  = '". $username ."'";
-            $queryResult = $mysqli->query($selectFirstQuery);
-            $foundRows = $queryResult->num_rows;
-            if($foundRows > 0)
-            {
-                
-                while($row=mysqli_fetch_assoc($queryResult)){
-                    $id=$row['id'];
-                }
-            }
-            $_SESSION['id']=$id;
-            $_SESSION['username']=$username;
-            $_SESSION['univid']=$univid;
-            $_SESSION['univeristy']=$university;
-            echo 1;
-        }
-	else
-        {
-	echo 4;
-        //sql isn't insserting the database
-			//	print $signupErrorLast;
-            
-        }
-}
-}
+    $_SESSION['fullname'] = "$_POST[name]";
+    $_SESSION['gender']= "$_POST[gender]";
+    $_SESSION['dob']= "$_POST[dob]";
+    $_SESSION['username'] = "$_POST[username]";//allow html to insert username
+    $_SESSION['email'] = "$_POST[email]";
+    $_SESSION['password'] = md5("$_POST[password]");//hashes password
+    $_SESSION['role'] = "$_POST[role]";
+	echo 0;
+
+
+
+
+
 }
 else
         {
