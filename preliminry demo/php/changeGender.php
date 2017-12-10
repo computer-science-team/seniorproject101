e7b483646a5c6494283bbbf05a35261a58df82f3
yousufahmed12 <thuinahmed@yahoo.com>
2017-12-09 22:23:09 -0500

    added ajax

--- preliminry demo/php/changeGender.php
+++ preliminry demo/php/changeGender.php
@@ -1,5 +1,91 @@
 <?php
 session_start();
+$role = ($_SESSION['role']);
+$role2 = "yes";
+$go = "";            
+            if (strcmp($role, $role2) !== 0){
+        		$go = "studentProfilePage.php";
+			
+			}
+		else{
+		$go ="facultyProfilePage.php";
+		    }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+
+<!------------------------------------------------------->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+
+	$(document).ready(function(){
+	$('#userForm').submit(function(){
+     
+	var Gender = $("#genders").val();
+	
+	if(Gender)
+{
+
+        $.post('changeGender_receiver.php', {gender: Gender}, function(data){
+	if (data == 0) {
+	//alert( data );
+
+	location.reload();
+
+        }
+
+        }).fail(function() {
+         
+            // just in case posting your form failed
+            alert( "Posting failed." );
+             
+        });
+}
+else
+{
+	 alert( "value is empty");
+}
+ 	
+        // to prevent refreshing the whole page page
+        return false;
+ 
+    });
+});
+</script>
+<!------------------------------------------------------->
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Edit Gender</title>
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+        <div class = "wrapper">
+		<div class="form-signin">
+		<div class="changeGender">
+			<h2>Change Gender</h2>
+			<form id='userForm'>
+			<p>Gender:</p>
+				<p>
+				<select id="genders" name="gender">
+  				<option value="Male">Male</option>
+  				<option value="Female">Female</option>
+  				<option value="Other">Other</option>
+				</select>
+				</p>
+			<input type='submit' value='Change Gender' />
+			<p><a href= "<?php echo $go ?>" >Profile</a></p>
+							
+				
+			</form>
+		</div>
+        </div>
+        </div>
+<?php
 $id = ($_SESSION['id']);
 $role = ($_SESSION['role']);
 $servername = "localhost";
@@ -8,18 +94,29 @@ $passwd = "";
 $dbname ="accounts";
 $mysqli =mysqli_connect($servername,$user,$passwd,$dbname);//login to database
 // Check connection
-
-if (isset($_POST['submit']))
-{
 if ($mysqli->connect_error) 
 {
     die("Connection failed: " . $conn->connect_error);
 }
-else 
+
+function ifsessionExists(){
+    //check if session exists?
+    if (isset($_SESSION['count'])){
+    return true;
+    }
+    else
+    {
+    return false;
+    }
+}
+ 
+if(ifsessionExists())
 {
-    //echo "Good";
-    //$username=$_POST['username'];
-    $password = $_POST['optradio'];
+    $count = '1';
+    if($_SESSION['count'] == $count)
+{
+$_SESSION['count'] = '0';
+    $password = $_SESSION['gender'];
     
 	 $sql= "UPDATE users SET gender= '".$password."' WHERE id = '".$id."'";
         
@@ -46,45 +143,5 @@ else
 }
 }//if isset
 ?>
-
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Edit Gender</title>
-		<link href="../css/bootstrap.min.css" rel="stylesheet">
-        <link href="../css/styles.css" rel="stylesheet">
-	</head>
-	<body>
-        <div class = "wrapper">
-		<div class="form-signin">
-		<div class="changeGender">
-			<h2>Change Gender</h2>
-			<form method="post" >
-
-			<p>Gender:
-				<label class="radio-inline">
-					<input type="radio" name="optradio"  value="Male"<?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Male' ) 
-					     echo ' checked="checked"';?> required>Male
-				</label>
-				<label class="radio-inline">
-					<input type="radio" name="optradio" value ="Female" <?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Female') 
-					     echo ' checked="checked"'; ?>required>Female
-				</label>
-				<label class="radio-inline">
-					<input type="radio" name="optradio" value="Other"  <?php if (isset($_POST['optradio']) && $_POST['optradio'] == 'Other') 
-					     echo ' checked="checked"'; ?>required>Other
-                </label></p>
-                <p>
-                    <input type="submit" name="submit" value="Change gender"></p>
-							
-				
-			</form>
-		</div>
-        </div>
-        </div>
 	</body>
 </html>
