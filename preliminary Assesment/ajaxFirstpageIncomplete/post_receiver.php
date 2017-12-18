e4224e307ae60676e7424b896c796e708e4ae327
Profs-CS-2018 <mcclintot4@students.rowan.edu>
2017-12-17 23:00:31 -0500

    Removal-TM

--- preliminary Assesment/ajaxFirstpageIncomplete/post_receiver.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-echo "<pre>";
-     $_SESSION['university'] = "$_POST[name]";
-	print_r($_SESSION['university']);
-echo "</pre>";
-?>
--- preliminary Assesment/ajaxFirstpageIncomplete/signup.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-	echo "Needs to add univid session token still work in progress session token university ";
-	print_r($_SESSION['university']);
-?>
